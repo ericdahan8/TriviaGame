aac6f825857b0bdb225e601be37ca86312be6165
Eric Dahan <Ericdahan@dyn-160-39-244-165.dyn.columbia.edu>
2019-02-14 20:45:10 -0500

    Added the questions and a submit button. Added radiobuttons too

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -3,15 +3,10 @@ var timeleft = 30;
 var timer = setInterval(function() {
   document.getElementById("progressBar").value = 30 - timeleft;
   timeleft -= 1;
-  if (timeleft <= 0) clearInterval(timer);
-}, 3000);
-
-var timeleft = 30;
-var timer = setInterval(function() {
   document.getElementById("countdown").innerHTML = timeleft + " seconds left";
-  timeleft -= 1;
   if (timeleft <= 0) {
     clearInterval(timer);
     document.getElementById("countdown").innerHTML = "Finished";
   }
-}, 3000);
+  if (timeleft <= 0) clearInterval(timer);
+}, 1000);
